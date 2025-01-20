d531c59812727ed3a5b4e5a5fa683517d3db0192
LukaGaganidze <lukagaganidzedev@gmail.com>
2025-01-21 01:16:20 +0400

    corrected stepper

--- src/app/features/book-consultation/book-consultation.component.ts
+++ src/app/features/book-consultation/book-consultation.component.ts
@@ -7,32 +7,4 @@ import { SharedModule } from '../../shared/shared.module';
   templateUrl: './book-consultation.component.html',
   styleUrl: './book-consultation.component.scss',
 })
-export class BookConsultationComponent {
-  activeStep: number = 1;
-
-  name: string | undefined | null = null;
-
-  email: string | undefined | null = null;
-
-  password: string | undefined | null = null;
-
-  option1: boolean | undefined = false;
-
-  option2: boolean | undefined = false;
-
-  option3: boolean | undefined = false;
-
-  option4: boolean | undefined = false;
-
-  option5: boolean | undefined = false;
-
-  option6: boolean | undefined = false;
-
-  option7: boolean | undefined = false;
-
-  option8: boolean | undefined = false;
-
-  option9: boolean | undefined = false;
-
-  option10: boolean | undefined = false;
-}
+export class BookConsultationComponent {}
