cd092ec07fd5cb518b8dc5730121e637d5cf2196
LukaGaganidze <lukagaganidzedev@gmail.com>
2025-01-23 02:16:16 +0400

    started responsive desktop first

---
+++ src/app/shared/global-interfaces/reservation.ts
@@ -0,0 +1,10 @@
+export interface ReservationInfo {
+  time: null | string;
+  doc: null | string;
+  speciality: null | string;
+  clinicName: null | string;
+  clinicAddress: null | string;
+  date: null | Date;
+  geoDate: null | string;
+  geoWeekDay: null | string;
+}
